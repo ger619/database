bfac866fde54f206bb23d6d09d9a9dc2e587ffff
David Abol Ger <draker74@gmail.com>
2022-07-09 14:30:05 +0300

    add-join

--- schema.sql
+++ schema.sql
@@ -1,49 +1,63 @@
--- Table: public.animals
 
--- DROP TABLE IF EXISTS public.animals;
+-- Create a table named animals with the specific columns
+CREATE TABLE animals (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50),
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL
+);
 
-CREATE TABLE IF NOT EXISTS public.animals
-(
-    id bigint,
-    name "char",
-    date_of_birth date,
-    escape_attempts bigint,
-    neutered boolean,
-    weight_kg double precision
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.animals
-    OWNER to postgres;
-
-ALTER TABLE animals ADD COLUMN species TEXT;
-
+-- Add a column species of type string to the animals
+ALTER TABLE animals ADD species VARCHAR(70);
 
 -- Create a table named owners with id, full_name, age.
-CREATE TABLE IF NOT EXISTS public.owners
-(
- id bigint GENERATED ALWAYS AS IDENTITY,
- full_name "char",
+CREATE TABLE owners(
+ id INT GENERATED ALWAYS AS IDENTITY,
+ full_name VARCHAR(100),
  age INT,
  PRIMARY KEY(id)
 );
 
 -- Create a table named species with id, name
 CREATE TABLE species(
- id bigint GENERATED ALWAYS AS IDENTITY,
- name "char",
+ id INT GENERATED ALWAYS AS IDENTITY,
+ name VARCHAR(80),
  PRIMARY KEY(id)
 );
 
-
 ALTER TABLE animals DROP COLUMN species;
 
-ALTER TABLE animals ADD COLUMN species_id bigint
+ALTER TABLE animals ADD COLUMN species_id INT
  REFERENCES species(id)
  ON DELETE CASCADE;
 
-ALTER TABLE animals ADD COLUMN owner_id bigint
+ALTER TABLE animals ADD COLUMN owner_id INT
  REFERENCES owners(id)
  ON DELETE CASCADE;
 
+ -- Create a table named vets.
+CREATE TABLE vets (
+	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(50),
+	age INT,
+	date_of_graduation DATE
+);
+
+-- Create a "join table" called specializations.
+CREATE TABLE specializations (
+	vets_id INT NOT NULL,
+	species_id INT NOT NULL,
+	FOREIGN KEY (vets_id) REFERENCES vets (id) ON UPDATE CASCADE,
+	FOREIGN KEY (species_id) REFERENCES species (id) ON UPDATE CASCADE
+);
+
+-- Create a "join table" called visits.
+CREATE TABLE visits (
+    animals_id INT NOT NULL,
+    vets_id INT NOT NULL,
+    date_of_visit DATE,
+    FOREIGN KEY (animals_id) REFERENCES animals(id) ON UPDATE CASCADE,
+    FOREIGN KEY (vets_id) REFERENCES vets(id) ON UPDATE CASCADE
+);
