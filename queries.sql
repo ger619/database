8de9485aa190c39a1041dcdd05bd79d8db2e2fd1
David Abol Ger <draker74@gmail.com>
2022-07-09 12:16:31 +0300

    multiple-tables

--- queries.sql
+++ queries.sql
@@ -1,29 +1,26 @@
-/*Queries that provide answers to the questions from all projects.*/
-
---  ends in "mon
+-- Find all animals whose name ends in "mon
 SELECT * FROM animals WHERE name LIKE '%mon';
 
 -- animals born between 2016 and 2019.
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 
---  animals that are neutered and have less than 3 escape attempts.
+-- animals that are neutered and have less than 3 escape attempts.
 SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
 
--- DOB of all animals named either "Agumon" or "Pikachu".
+--animals named either "Agumon" or "Pikachu".
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 
--- escape attempts of animals that weigh > 10.5kg
+--  name and escape attempts of animals that weigh > 10.5kg
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 
--- all neutered.
+-- all animals neutered.
 SELECT * FROM animals WHERE neutered = true;
 
--- animals not named Gabumon.
+-- all animals not named Gabumon.
 SELECT * FROM animals WHERE name != 'Gabumon';
 
--- animals with a weight between 10.4kg and 17.3kg
+-- all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
-
 
 BEGIN;
 UPDATE animals SET species = 'unspecified';
@@ -31,13 +28,11 @@ SELECT * FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
 
-
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 COMMIT;
 SELECT * FROM animals;
-
 
 BEGIN;
 DELETE FROM animals;
@@ -53,10 +48,39 @@ ROLLBACK TO update_weight;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
 
-
 SELECT COUNT(*) FROM animals;
 SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
 SELECT AVG(weight_kg) FROM animals;
 SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY (neutered);
 SELECT species, MIN(weight_kg), MAX(weight_kg)FROM animals GROUP BY(species);
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY(species);
+
+
+SELECT animals.* FROM animals
+ JOIN owners ON animals.owner_id = owners.id
+ WHERE owners.full_name = 'Melody Pond';
+
+SELECT animals.* FROM animals
+ JOIN species ON animals.species_id = species.id
+ WHERE species.name = 'Pokemon';
+
+SELECT owners.full_name,animals.name FROM owners
+ LEFT JOIN animals ON owners.id = animals.owner_id;
+
+SELECT species.name, COUNT(*) FROM animals
+ JOIN species ON animals.species_id = species.id
+ GROUP BY species.name;
+
+SELECT animals.* FROM animals
+ JOIN owners ON animals.owner_id = owners.id
+ JOIN species ON animals.species_id = species.id
+ WHERE owners.full_name = 'Jennifer Orwell' AND species.name = 'Digimon';
+
+SELECT animals.* FROM animals
+ JOIN owners ON animals.owner_id = owners.id
+ WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0;
+
+SELECT owners.full_name,COUNT(*) FROM owners
+ JOIN animals ON owners.id = animals.owner_id
+ GROUP BY owners.full_name
+ ORDER BY count DESC LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -20,3 +20,30 @@ ALTER TABLE IF EXISTS public.animals
 ALTER TABLE animals ADD COLUMN species TEXT;
 
 
+-- Create a table named owners with id, full_name, age.
+CREATE TABLE IF NOT EXISTS public.owners
+(
+ id bigint GENERATED ALWAYS AS IDENTITY,
+ full_name "char",
+ age INT,
+ PRIMARY KEY(id)
+);
+
+-- Create a table named species with id, name
+CREATE TABLE species(
+ id bigint GENERATED ALWAYS AS IDENTITY,
+ name "char",
+ PRIMARY KEY(id)
+);
+
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id bigint
+ REFERENCES species(id)
+ ON DELETE CASCADE;
+
+ALTER TABLE animals ADD COLUMN owner_id bigint
+ REFERENCES owners(id)
+ ON DELETE CASCADE;
+
